80135df08f882c8b26bc70e0e8200761bbec3458
zipsterific <alan.ebbert@gmail.com>
2017-11-05 16:03:06 -0600

    Added neovim repo and install

--- all-software.sh
+++ all-software.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get upgrade -
+
 sudo apt-get install -y python-pip     #python 2
 sudo apt-get install -y python3-pip    #python 3
 sudo -H /usr/bin/python -m pip install pylint
 sudo -H pip install --upgrade pip 
 sudo -H pip install setuptools
+
+sudo add apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
