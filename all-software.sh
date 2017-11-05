9180e2c5c13bfe8e9013a30750338dd4a923d702
zipsterific <alan.ebbert@gmail.com>
2017-11-05 15:28:09 -0600

    Added os upgrade

--- all-software.sh
+++ all-software.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get install -y python-pip     #python 2
 sudo apt-get install -y python3-pip    #python 3
 sudo /usr/bin/python -m pip install pylint
