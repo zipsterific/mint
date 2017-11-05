fb5a46f9590797e13b849ed33730301e9a9c14bd
zipsterific <alan.ebbert@gmail.com>
2017-11-05 15:48:25 -0600

    Added -H sudo option for pip installs

--- all-software.sh
+++ all-software.sh
@@ -5,5 +5,5 @@ sudo apt-get upgrade -y
 sudo apt-get install -y python-pip     #python 2
 sudo apt-get install -y python3-pip    #python 3
 sudo /usr/bin/python -m pip install pylint
-sudo pip install --upgrade pip 
-sudo pip install setuptools
+sudo -H pip install --upgrade pip 
+sudo -H pip install setuptools
