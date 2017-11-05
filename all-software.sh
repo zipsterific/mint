6d36e7d41f44aff445f26e2e592e5ba1e6bb4a0d
zipsterific <alan.ebbert@gmail.com>
2017-11-05 16:45:22 -0600

    more stuff

--- all-software.sh
+++ all-software.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade -
+# update the system
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 sudo apt-get install -y python-pip     #python 2
 sudo apt-get install -y python3-pip    #python 3
@@ -9,6 +10,16 @@ sudo -H /usr/bin/python -m pip install pylint
 sudo -H pip install --upgrade pip 
 sudo -H pip install setuptools
 
-sudo add-apt-repository -y ppa:neovim-ppa/stable
+# software from 'normal' repositories
+sudo apt-get install -y catfish clementine curl dconf-cli dconf-editor dropbox evolution focuswriter geany geary gimp gpick
+sudo apt-get install -y glances gparted grsync hardinfo inkscape meld
+sudo apt-get install -y openshot pinta plank ppa-purge radiotray screenruler screenfetch scrot shutter slurm synapse
+sudo apt-get install -y thunar vlc vnstat
+
+# installation of zippers and unzippers
+sudo apt-get install -y p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller
+
+# some of the unusual stuff
+sudo apsudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install -y neovim
