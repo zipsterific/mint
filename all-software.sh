68778e9f16e09473f1d852163b570f61dff1df7d
zipsterific <alan.ebbert@gmail.com>
2017-11-05 15:50:49 -0600

    sudo H option added to pylint install

--- all-software.sh
+++ all-software.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y python-pip     #python 2
 sudo apt-get install -y python3-pip    #python 3
-sudo /usr/bin/python -m pip install pylint
+sudo -H /usr/bin/python -m pip install pylint
 sudo -H pip install --upgrade pip 
 sudo -H pip install setuptools
