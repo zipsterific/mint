8cc005b6f6653cf80591adb4a2a4f81455dbe194
zipsterific <alan.ebbert@gmail.com>
2017-11-08 19:14:55 -0600

    added etcher install flag

--- install-etcher.sh
+++ install-etcher.sh
@@ -15,4 +15,4 @@ set -e
 
 echo "deb https://dl.bintray.com/resin-io/debian stable etcher" | sudo tee /etc/apt/sources.list.d/etcher.list
 apt-get update
-apt-get install -y etcher-electron
+apt-get install -y --allow-unauthenticated etcher-electron
