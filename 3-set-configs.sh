06d067d852eb1b3503c74ae4b5bed7e105c4a9d2
Alan <alan@Mini-Me.home.local>
2017-11-18 14:13:39 -0600

    added idrive to 3-set-configs

--- 3-set-configs.sh
+++ 3-set-configs.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+idrive=/home/alan/idrive
+
+mkdir $idrive
+wget https://www.idrivedownloads.com/downloads/linux/download-for-linux/IDrive_for_Linux.zip -O /$idrive
+unzip $idrive/idrive.zip
+chmod a+x $idrive/IDrive_for_Linux/scripts/*.pl
