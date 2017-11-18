df175d86694898e3ae2faf53a2d4fc6f2d80cedd
Alan <alan@Mini-Me.home.local>
2017-11-18 14:30:40 -0600

    idrive exit code

--- 3-set-configs.sh
+++ 3-set-configs.sh
@@ -4,7 +4,12 @@
 
 idrive=/home/alan/idrive
 
-mkdir $idrive
+if [ ! -d $idrive ]; then
+    mkdir $idrive
+    else
+    echo "It appears the iDrive files are already installed. Aborted."
+    exit 1
+fi
 wget https://www.idrivedownloads.com/downloads/linux/download-for-linux/IDrive_for_Linux.zip -O /$idrive/idrive.zip
 unzip $idrive/idrive.zip
 chmod a+x $idrive/IDrive_for_Linux/scripts/*.pl
