805e5804e7d4b69c4d39783ab543a171a4e57946
Alan <alan@Mini-Me.home.local>
2017-11-18 14:23:06 -0600

    fixed idrive zip location

--- 3-set-configs.sh
+++ 3-set-configs.sh
@@ -5,6 +5,6 @@
 idrive=/home/alan/idrive
 
 mkdir $idrive
-wget https://www.idrivedownloads.com/downloads/linux/download-for-linux/IDrive_for_Linux.zip -O /$idrive
+wget https://www.idrivedownloads.com/downloads/linux/download-for-linux/IDrive_for_Linux.zip -O /$idrive/idrive.zip
 unzip $idrive/idrive.zip
 chmod a+x $idrive/IDrive_for_Linux/scripts/*.pl
