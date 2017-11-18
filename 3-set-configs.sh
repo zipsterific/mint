904dd21f11dad551f3ce17e05b14545bcdd0e833
Alan <alan@Mini-Me.home.local>
2017-11-18 14:40:44 -0600

    commit

--- 3-set-configs.sh
+++ 3-set-configs.sh
@@ -10,6 +10,6 @@ if [ ! -d $idrive ]; then
     echo "It appears the iDrive files are already installed. Aborted."
     exit 1
 fi
-wget https://www.idrivedownloads.com/downloads/linux/download-for-linux/IDrive_for_Linux.zip -O /$idrive/idrive.zip
+wget https://www.idrivedownloads.com/downloads/linux/download-for-linux/IDrive_for_Linux.zip -O $idrive/idrive.zip
 unzip $idrive/idrive.zip
 chmod a+x $idrive/IDrive_for_Linux/scripts/*.pl
