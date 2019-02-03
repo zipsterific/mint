29bfaebc7c85b0c8bf00ba1b629d6f0664e704fa
Al <alan.ebbert@gmail.com>
2019-02-03 10:05:10 -0600

    Update install-vagrant.sh

--- install-vagrant.sh
+++ install-vagrant.sh
@@ -13,6 +13,7 @@ set -e
 
 # Vagrant
 
-wget https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb -O /tmp/vagrant.deb
+wget https://releases.hashicorp.com/vagrant/2.2.3/vagrant_2.2.3_x86_64.deb -O /tmp/vagrant.deb
+# wget https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb -O /tmp/vagrant.deb
 dpkg -i /tmp/vagrant.deb
 rm /tmp/vagrant.deb
