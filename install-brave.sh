53c16a07133cafedf6db3569f3bc4772e811b2aa
Al <alan.ebbert@gmail.com>
2019-02-03 20:14:26 -0600

    Update install-brave.sh

--- install-brave.sh
+++ install-brave.sh
@@ -14,7 +14,8 @@ set -e
 # Brave
 
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-source /etc/os-release
+# source /etc/os-release #don't use 'source' use . to source
+. /etc/os-release
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list
 sudo apt update
 sudo apt install brave-browser brave-keyring
